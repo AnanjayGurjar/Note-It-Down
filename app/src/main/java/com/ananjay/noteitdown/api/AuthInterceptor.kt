c39e1fffde0be1beff07eba12fd746a4de312b57
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-06-30 22:46:03 +0530

    added interceptor for token

---
+++ app/src/main/java/com/ananjay/noteitdown/api/AuthInterceptor.kt
@@ -0,0 +1,23 @@
+package com.ananjay.noteitdown.api
+
+import com.ananjay.noteitdown.utils.TokenManager
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(): Interceptor {
+
+    @Inject
+    lateinit var tokenManager: TokenManager
+    //this method observe the request sent to the api and we can do operations on it
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = tokenManager.getToken()
+        val request = chain.request()
+            .newBuilder()
+            .addHeader("Authorization", "Bearer $token")
+            .build()
+
+
+        return chain.proceed(request)
+    }
+}
--- app/src/main/java/com/ananjay/noteitdown/di/NetworkModule.kt
+++ app/src/main/java/com/ananjay/noteitdown/di/NetworkModule.kt
@@ -1,11 +1,15 @@
 package com.ananjay.noteitdown.di
 
+import com.ananjay.noteitdown.api.AuthInterceptor
+import com.ananjay.noteitdown.api.NotesApi
 import com.ananjay.noteitdown.api.UserApi
 import com.ananjay.noteitdown.utils.AppConstants.Companion.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,16 +20,30 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun getRetrofit(): Retrofit{
+    fun getRetrofit(): Retrofit.Builder {
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
-            .build()
     }
 
     @Singleton
     @Provides
-    fun providesUserApi(retrofit: Retrofit): UserApi{
-        return retrofit.create(UserApi::class.java)
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor) : OkHttpClient{
+        return OkHttpClient.Builder().addInterceptor(authInterceptor).build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesUserApi(retrofitBuilder: Retrofit.Builder): UserApi{
+        return retrofitBuilder.build().create(UserApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun providesNotesApi(retrofitBuilder: Retrofit.Builder, okHttpClient: OkHttpClient): NotesApi{
+        return retrofitBuilder
+            .client(okHttpClient)
+            .build()
+            .create(NotesApi::class.java)
     }
 }
